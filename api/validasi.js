f31f08f98d809ce33e055c4daf03ffa75749cb7f
Adirangga <127793665+adiranggax@users.noreply.github.com>
2026-03-13 02:09:47 +0800

    Update validasi.js

--- api/validasi.js
+++ api/validasi.js
@@ -1,25 +1,27 @@
-const validasi = require("../lib/validasi");
+process.removeAllListeners('warning')
+
+const validasi = require("../lib/validasi")
 
 module.exports = async function(req,res){
 
-const { id, serverid } = req.query;
+const { id, serverid } = req.query
 
 try{
 
-const data = await validasi(id,serverid);
+const data = await validasi(id,serverid)
 
 res.json({
 status:true,
-nickname:data["in-game-nickname"] || data.nickname || "Unknown",
-country:data.country || "Unknown"
-});
+nickname:data["in-game-nickname"] || data.nickname,
+country:data.country
+})
 
 }catch(err){
 
 res.json({
 status:false,
 message:err.toString()
-});
+})
 
 }
 
